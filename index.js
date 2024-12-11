350f62761a1ef1becd628191e462156a5e75cbf3
genos-ux <gabriel.kyeremateng.gk@gmail.com>
2024-12-11 10:30:49 +0000

    Applied css to the grid container

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 const container = document.querySelector("#container");
-container.style.cssText = "display: flex;border:1px solid black;flex-wrap:wrap";
+const btn = document.querySelector("button");
+let gridSize = 0;
+
 
 // container.style = "border: 1px solid black;"
 // container.innerHTML = `<p>Hello world</p>`;
 
-for(let i = 0; i < 256; i++)
-{
-    const miniDiv = document.createElement('div');
-    miniDiv.style.cssText = "border:1px solid black;width:30px;height: 30px;margin:10px;flex-basis: 4.5%;";
+btn.addEventListener('click',()=> {
 
-    // miniDiv.addEventListener()
+    let result = +prompt("Enter the grid size: ");
 
-    miniDiv.addEventListener('mouseover',(event)=> {
-        event.target.style.cssText += "background: black;";
-    })
+    if(result == result && result != 0)
+    {
+        gridSize = result;
+        // console.log(result);
 
-    container.appendChild(miniDiv);
-}
+        for(let i = 0; i < gridSize * gridSize; i++)
+        {
+            const gridItem = document.createElement('div');
+            gridItem.style.cssText = "border:1px solid black;width:30px;height: 30px;margin:10px;flex-basis: 9%;";
+            gridItem.classList.add('grid-item');
+
+            // gridItem.addEventListener()
+
+            gridItem.addEventListener('mouseover',(event)=> {
+                event.target.style.cssText += "background: black;";
+            })
+
+            container.appendChild(gridItem);
+        }
+
+        //Apply CSS to the grid container and grid items.
+        container.style.cssText = `display: flex;border:1px solid black;flex-wrap:wrap;width: ${gridSize * 10}px;height: ${gridSize * 10}px`;
+    }
+
+    else
+    {
+        alert("Please enter a valid digit.");
+    }
+
+})
